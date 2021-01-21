d859876bd30b556ea781162d06d148077010ea1c
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-21 17:13:14 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from pyrogram import Client, Filters
 
 
-@Client.on_message(Filters.command(["ping", "ping@xploaderprobot"]))
+@Client.on_message(Filters.command(["ping", "ping@ziggyuploaderbot"]))
 async def ping(client, message):
     start_time = int(round(time.time() * 1000))
     event = await message.reply_text("Pingy Pongy Pong Pong Ping Comes Now")
